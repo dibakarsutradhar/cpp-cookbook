71195bc05e7c80fa719d8c59949cabbdcdecd90c
Dibakar <crossbonesdipu@gmail.com>
2018-11-29 23:05:11 +0600

    georgeringo files

---
+++ 1.building-cpp-application/3.dynamicLibrary/george.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "george.hpp"
+
+using namespace std;
+
+int main(){
+    cout << "George, ";
+}
---
+++ 1.building-cpp-application/3.dynamicLibrary/ringo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ringo.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "and Ringo\n";
+}
